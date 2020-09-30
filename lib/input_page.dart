d233e4e9f57338b8c9ae72a78065bccab069a17e
Karim Elbayaa <kareemelbayaa@gmail.com>
2020-09-30 04:51:35 +0200

    gender ui selection
    Signed-off-by: Karim Elbayaa <kareemelbayaa@gmail.com>

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,15 +4,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+var _accentColor = Color(0xFFEB1555);
+var _bottomContainerHeight = 80.0;
+var _activeCardColor = Color(0xFF1D1E33);
+const _inactiveCardColor = Color(0xFF111328);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
-  var _accentColor = Color(0xFFEB1555);
-  var _bottomContainerHeight = 80.0;
-  var _activeCardColor = Color(0xFF1D1E33);
+
+  Color maleCardColor = _inactiveCardColor;
+  Color femaleCardColor = _inactiveCardColor;
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +32,25 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: GestureDetector(
+                    onTap: () { // classic anonymous callbacks
+                      setState(() {
+                        updateColor(1);
+                      });
+                    },
                     child: ReusableCard(
-                        color: _activeCardColor,
+                        color: maleCardColor,
                         cardChild: IconContent(FontAwesomeIcons.mars, 'Male')),
                   ),
                 ),
                 Expanded(
                   child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        updateColor(2);
+                      });
+                    },
                     child: ReusableCard(
-                      color: _activeCardColor,
+                      color: femaleCardColor,
                       cardChild: IconContent(FontAwesomeIcons.venus, 'Female'),
                     ),
                   ),
@@ -67,9 +82,35 @@ class _InputPageState extends State<InputPage> {
               height: _bottomContainerHeight,
               width: double.infinity,
               child: FlatButton(
-                  onPressed: () {}, child: Text('Calculate your BMI')),
+                  onPressed: () {
+
+                  }, child: Text('Calculate your BMI')),
             )
           ],
         ));
+  }
+  void updateColor(int gender)
+  {
+    if(gender==1)
+    {
+      if(maleCardColor==_inactiveCardColor)
+      {
+        maleCardColor = _activeCardColor;
+        femaleCardColor = _inactiveCardColor;
+      }else
+        {
+          maleCardColor = _inactiveCardColor;
+        }
+    }else if(gender==2)
+      {
+        if(femaleCardColor==_inactiveCardColor)
+        {
+          femaleCardColor=_activeCardColor;
+          maleCardColor = _inactiveCardColor;
+        }else
+          {
+            femaleCardColor=_inactiveCardColor;
+          }
+      }
   }
 }
